ab32b2a9029b73501a533382164fd232fe2bc9ec
Marina Kolesnikova <MarinaKo@playtika.com>
2022-11-27 18:18:34 +0200

    models and test data provided

--- djangoapp/models.py
+++ djangoapp/models.py
@@ -1,10 +1,31 @@
 from __future__ import unicode_literals
 from django.db import models
 
-class Measurement(models.Model):
- value = models.CharField(max_length=50)
 
- description = models.CharField(max_length=250, blank=True, null=True) 
+class Temperature (models.Model):
+    value = models.IntegerField()
+
+
+class Humidity (models.Model):
+    value = models.DecimalField(
+        decimal_places=2, max_digits=6)
+
+
+class Pressure (models.Model):
+    value = models.DecimalField(decimal_places=2, max_digits=7)
+
+
+class PeriodicMeasurements (models.Model):
+    temperature = models.ForeignKey(Temperature, on_delete=models.CASCADE)
+    humidity = models.ForeignKey(Humidity, on_delete=models.CASCADE)
+    pressure = models.ForeignKey(Pressure, on_delete=models.CASCADE)
+    dateTime = models.DateTimeField(auto_now_add=True)
+
+
+class Unit (models.Model):
+    unit = models.CharField(max_length=50)
+    measurement = models.CharField(unique=True, max_length=50)
+
 
 def __unicode__(self):
- return self.value
+    return self.value
--- djangoapp/views.py
+++ djangoapp/views.py
@@ -1,17 +1,34 @@
 import os
 from django.views import generic
 from django.conf import settings
-from djangoapp.models import Measurement
+
+from djangoapp.models import PeriodicMeasurements, Temperature, Humidity, Pressure
+
+
 class MeasurementList(generic.ListView):
- model = Measurement
- context_object_name = 'measurements'
- template_name = 'measurement_list.html'
- def get(self, request, *args, **kwargs):
-    try:
-        file_path =os.path.join(settings.BASE_DIR, 'test_data')
-        measurement_file = open(file_path,'r')
-        for value in measurement_file:
-            Measurement.objects.get_or_create(value=value)
-    except IOError:
-        pass
-    return super(MeasurementList,self).get(request, *args, **kwargs)
+    model = PeriodicMeasurements
+    context_object_name = 'measurements'
+    template_name = 'measurement_list.html'
+
+    def openFile(self, fileName):
+        return open(os.path.join(
+            settings.BASE_DIR, fileName), 'r')
+
+    def get(self, request, *args, **kwargs):
+        try:
+            temperature_file = self.openFile('temperature_test')
+            humidity_file = self.openFile('humidity_test')
+            pressure_file = self.openFile('pressure_test')
+            for temper, hum, pr in zip(temperature_file, humidity_file, pressure_file):
+                tempr = Temperature.objects.get_or_create(
+                    value=temper)
+                humid = Humidity.objects.get_or_create(
+                    value=hum)
+                press = Pressure.objects.get_or_create(
+                    value=pr)
+                PeriodicMeasurements.objects.get_or_create(
+                    temperature=tempr[0], humidity=humid[0], pressure=press[0])
+
+        except IOError:
+            pass
+        return super(MeasurementList, self).get(request, *args, **kwargs)
