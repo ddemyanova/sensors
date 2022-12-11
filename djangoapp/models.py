6db19f32e7fede129bb043067e5042c48b8beb45
Marina Kolesnikova <MarinaKo@playtika.com>
2022-12-12 00:33:30 +0200

    model changed

--- djangoapp/models.py
+++ djangoapp/models.py
@@ -2,29 +2,11 @@ from __future__ import unicode_literals
 from django.db import models
 
 
-class Temperature (models.Model):
-    value = models.IntegerField()
-
-
-class Humidity (models.Model):
-    value = models.DecimalField(
-        decimal_places=2, max_digits=6)
-
-
-class Pressure (models.Model):
-    value = models.DecimalField(decimal_places=2, max_digits=7)
-
-
 class PeriodicMeasurements (models.Model):
-    temperature = models.ForeignKey(Temperature, on_delete=models.CASCADE)
-    humidity = models.ForeignKey(Humidity, on_delete=models.CASCADE)
-    pressure = models.ForeignKey(Pressure, on_delete=models.CASCADE)
+    temperature = models.IntegerField()
+    humidity = models.DecimalField(decimal_places=2, max_digits=6)
+    pressure = models.DecimalField(decimal_places=2, max_digits=7)
     dateTime = models.DateTimeField(auto_now_add=True)
-
-
-class Unit (models.Model):
-    unit = models.CharField(max_length=50)
-    measurement = models.CharField(unique=True, max_length=50)
 
 
 def __unicode__(self):
--- djangoapp/views.py
+++ djangoapp/views.py
@@ -2,7 +2,7 @@ import os
 from django.views import generic
 from django.conf import settings
 
-from djangoapp.models import PeriodicMeasurements, Temperature, Humidity, Pressure
+from djangoapp.models import PeriodicMeasurements
 
 
 class MeasurementList(generic.ListView):
@@ -19,15 +19,9 @@ class MeasurementList(generic.ListView):
             temperature_file = self.openFile('temperature_test')
             humidity_file = self.openFile('humidity_test')
             pressure_file = self.openFile('pressure_test')
-            for temper, hum, pr in zip(temperature_file, humidity_file, pressure_file):
-                tempr = Temperature.objects.get_or_create(
-                    value=temper)
-                humid = Humidity.objects.get_or_create(
-                    value=hum)
-                press = Pressure.objects.get_or_create(
-                    value=pr)
+            for temp, hum, pr in zip(temperature_file, humidity_file, pressure_file):
                 PeriodicMeasurements.objects.get_or_create(
-                    temperature=tempr[0], humidity=humid[0], pressure=press[0])
+                    temperature=temp, humidity=hum, pressure=pr)
 
         except IOError:
             pass
