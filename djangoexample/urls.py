06d237a9a875e8562a4661208dc3bce529fb8a91
Marina Kolesnikova <MarinaKo@playtika.com>
2023-01-30 01:34:13 +0200

    analytics provided

--- djangoexample/urls.py
+++ djangoexample/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path, include
-from djangoapp.views import MeasuresListView
+from djangoapp.views import MeasuresListView, analytics_view
 from django.contrib import admin
 
 urlpatterns = [
     path('api/', include('djangoapp.urls')),
     path('measurements/', MeasuresListView.as_view(),  name='measurements-list'),
+    path('analytics/', analytics_view,  name='analytics'),
+
     path('admin/', admin.site.urls),
 ]
